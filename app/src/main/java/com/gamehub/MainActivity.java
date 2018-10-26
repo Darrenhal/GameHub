a98a79068f46c4862ef1d88a94302b96b762b395
TWwifi <nholli@web.de>
2018-10-26 22:11:22 +0200

    Hello World Programm

--- app/src/main/java/com/gamehub/MainActivity.java
+++ app/src/main/java/com/gamehub/MainActivity.java
@@ -2,12 +2,27 @@ package com.gamehub;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonTest(View view) {
+        long millis = System.currentTimeMillis();
+        TextView tView = findViewById(R.id.textView);
+        tView.setText("Hello World: " + millis);
+    }
+
+    public void onTTTClick(View view) {
+
+        this.view = view;
     }
 }

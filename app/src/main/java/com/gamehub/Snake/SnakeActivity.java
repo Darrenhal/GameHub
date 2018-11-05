08c4bf12f26ac83f685490dbdb3fb28502c0bea1
Gary <gary.hoeppner@t-online.de>
2018-11-05 21:40:45 +0100

    Snake erste Version...Grundversion, dass Programm ausführbar

--- app/src/main/java/com/gamehub/Snake/SnakeActivity.java
+++ app/src/main/java/com/gamehub/Snake/SnakeActivity.java
@@ -1,15 +1,33 @@
 package com.gamehub.Snake;
 
 import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 
 import com.gamehub.R;
 
 public class SnakeActivity extends Activity {
 
+    SnakeEngine snakeEngine;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_snake);
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        snakeEngine = new SnakeEngine(this, size);
+        setContentView(snakeEngine);
+    }
+
+     protected void onResume(){
+        super.onResume();
+        snakeEngine.resume();
+    }
+
+    protected void onPause(){
+        super.onPause();
+        snakeEngine.onPause();
     }
 }

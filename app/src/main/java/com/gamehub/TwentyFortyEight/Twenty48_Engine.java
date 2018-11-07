7095e92bd44ebd4b76d1753a3f034cb06079c545
TWwifi <nholli@web.de>
2018-11-07 01:25:51 +0100

    code cleanup for 2048

--- app/src/main/java/com/gamehub/TwentyFortyEight/Twenty48_Engine.java
+++ app/src/main/java/com/gamehub/TwentyFortyEight/Twenty48_Engine.java
@@ -1,5 +1,6 @@
 package com.gamehub.TwentyFortyEight;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -12,73 +13,182 @@ import android.view.SurfaceView;
 
 public class Twenty48_Engine extends SurfaceView implements Runnable {
 
+    // logic variables to run the game
+    private boolean isInitialized;
     private boolean isPlaying;
     private long nextFrame;
     private int high_score;
     private int score;
-
-    private Thread thread;
     private Paint paint;
     private SurfaceHolder surfaceHolder;
 
-    // detect swipes
+    // variables to handle swipe detection
     private float downX, downY, upX, upY;
     private boolean isBtnDown, isBtnUp;
 
-    // store coordinates for tiles
+    // arrays to track board and game information
     private Twenty48_Coordinate[] tileCoordinateArray;
-
-    // store information about tiles
+    private Twenty48_UiElement[] uiElementArray;
     private Twenty48_Tile[] tileArray;
     private boolean[] isTileSetArray;
 
     public Twenty48_Engine(Context context) {
         super(context);
-        Log.d("Sys out", "########## GO ##########");
+        Log.d("Syso", "########## GO ##########");
 
-        this.score = 0;
-        this.surfaceHolder = getHolder();
-        this.paint = new Paint();
-        this.tileCoordinateArray = new Twenty48_Coordinate[16];
-        this.tileArray = new Twenty48_Tile[16];
-        this.isTileSetArray = new boolean[16];
-
+        score = 0;
+        high_score = 0; // hardcoded for now
+        nextFrame = 0;  // first frame will be drawn immediately after thread start
         downX = downY = upX = upY = 0.0f;
         isBtnDown = isBtnUp = false;
+        isInitialized = false;
 
-        // ToDo High Score handling
-        // ToDo for now it will be hardcoded
-        // ToDO change to retrieve high score from database
-        this.high_score = 0;
+        surfaceHolder = getHolder();
+        paint = new Paint();
 
-        this.nextFrame = System.currentTimeMillis() + 40;
+        tileCoordinateArray = new Twenty48_Coordinate[16];
+        tileArray = new Twenty48_Tile[16];
+        isTileSetArray = new boolean[16];
+        uiElementArray = new Twenty48_UiElement[4];
 
-        this.startGame();
+        for(int i = 0; i < 16; i++) {
+            isTileSetArray[i] = false;
+        }
     }
 
-    private void startGame() {
-        // initialize empty board
-        for(int i = 0; i < isTileSetArray.length; i++) {
-            isTileSetArray[i] = false;
-            tileArray[i] = null;
+    private void initialize() {
+        if (surfaceHolder.getSurface().isValid()) {
+            Canvas canvas = surfaceHolder.lockCanvas();
+            int height = canvas.getHeight();
+            int width = canvas.getWidth();
+
+            // saving the coordinates for the score board
+            Twenty48_UiElement title = new Twenty48_UiElement((int) (width * 0.1), (int) (height / 4.5), 0, 0);
+            Twenty48_UiElement score = new Twenty48_UiElement((int) (width * 0.51), (height / 8), (int) (width * 0.7), (height / 5));
+            Twenty48_UiElement highScore = new Twenty48_UiElement((int) (width * 0.71), (height / 8), (int) (width * 0.9), (height / 5));
+            uiElementArray[1] = title;
+            uiElementArray[2] = score;
+            uiElementArray[3] = highScore;
+
+            // get coordinates for each tile on the board
+            int boardWidth = (int) (width * 0.8);
+            int border = ((width - boardWidth) / 2);
+            int tileLength = (int) (boardWidth / 4.5);
+            int tileOffset = ((boardWidth - (tileLength * 4)) / 5);
+            int boardY = (height - boardWidth - border);
+            int tileStartX, tileStartY, tileEndX, tileEndY;
+            // save board coordinates as uiElement
+            uiElementArray[0] = new Twenty48_UiElement(border, boardY, (border + boardWidth), (boardY + boardWidth));
+
+            // generate board coordinates and save them
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4 ; j++) {
+                    tileStartX = border + (tileOffset * (j + 1)) + (tileLength * j);
+                    tileStartY = boardY + (tileOffset * (i + 1)) + (tileLength * i);
+                    tileEndX = border + (tileOffset * (j + 1)) + (tileLength * (j + 1));
+                    tileEndY = boardY + (tileOffset * (i + 1)) + (tileLength * (i + 1));
+                    tileCoordinateArray[(i * 4) + j] = new Twenty48_Coordinate(tileStartX, tileStartY, tileEndX, tileEndY);
+                }
+            }
+            surfaceHolder.unlockCanvasAndPost(canvas);
+            spawnTile();
+            isInitialized = true;
         }
     }
 
     private void spawnTile() {
-        isTileSetArray[7] = true;
-        tileArray[7] = new Twenty48_Tile(tileCoordinateArray[7].startX, tileCoordinateArray[7].startY, tileCoordinateArray[7].endX, tileCoordinateArray[7].endY, 2);
-        isTileSetArray[13] = true;
-        tileArray[13] = new Twenty48_Tile(tileCoordinateArray[13].startX, tileCoordinateArray[13].startY, tileCoordinateArray[13].endX, tileCoordinateArray[13].endY, 4);
+        int rmdValue;
+        if ((Math.random() * 100) < 60) {
+            rmdValue = 2;
+        } else {
+            rmdValue = 4;
+        }
+        int rmdPosition;
+        // checks for unset tile
+        // -> loops until random unset position (if many) is found
+        // -> creates tile on random position and returns
+        // if no unset tile is found, endGame will be called
+        for (int i = 0; i < isTileSetArray.length; i++) {
+            if (!isTileSetArray[i]) {
+                while(true) {
+                    rmdPosition = (int) (Math.random() * 16);
+                    if (!isTileSetArray[rmdPosition]) {
+                        isTileSetArray[rmdPosition] = true;
+                        Twenty48_Coordinate c = tileCoordinateArray[rmdPosition];
+                        tileArray[rmdPosition] = new Twenty48_Tile(c.startX, c.startY, c.endX, c.endY, rmdValue);
+                        return;
+                    }
+                }
+            }
+        }
+        endGame();
     }
 
     private void update() {
-        if (this.score > this.high_score) {
-            this.high_score = this.score;
+        if (score > high_score) {
+            high_score = score;
         }
         if (isBtnDown && isBtnUp) {
             isBtnDown = isBtnUp = false;
             detectSwipe();
         }
+    }
+
+    private void draw() {
+        if  (this.surfaceHolder.getSurface().isValid()) {
+            if (isInitialized) {
+                Canvas canvas = this.surfaceHolder.lockCanvas();
+
+                // draw uiElements
+                Twenty48_UiElement board = uiElementArray[0];
+                Twenty48_UiElement title = uiElementArray[1];
+                Twenty48_UiElement score = uiElementArray[2];
+                Twenty48_UiElement best = uiElementArray[3];
+
+                // paint uiElements
+                paint.setColor(Color.argb(255, 100, 100, 100));
+                canvas.drawRect(board.startX, board.startY, board.endX, board.endY, paint);
+                paint.setColor(Color.argb(255, 216, 216, 216));
+                canvas.drawRect(score.startX, score.startY, score.endX, score.endY, paint);
+                canvas.drawRect(best.startX, best.startY, best.endX, best.endY, paint);
+                // write score board text
+                paint.setColor(Color.BLACK);
+                paint.setTextSize(40f);
+                paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+                canvas.drawText("Score", score.startX, score.startY - 7, paint);
+                canvas.drawText("Best", best.startX, best.startY-  7, paint);
+                paint.setTextSize(128f);
+                canvas.drawText("0x800", title.startX, title.startY, paint);
+                // draw tiles to board
+                for (int i = 0; i < tileArray.length; i++) {
+                    if (tileArray[i] != null) {
+                        Twenty48_Tile tile = tileArray[i];
+                        // draw colored tile
+                        paint.setColor(Color.argb(255, tile.color.red, tile.color.green, tile.color.blue));
+                        canvas.drawRect(tile.startX, tile.startY, tile.endX, tile.endY, paint);
+                        // draw number to tile
+                        paint.setColor(Color.DKGRAY);
+                        paint.setTextSize(80f);
+                        paint.setTextAlign(Paint.Align.CENTER);
+                        float textX = tile.startX + ((tile.endX - tile.startX) / 2);
+                        float textY = tile.startY + ((tile.endY - tile.startY) / 2) - (paint.descent() +
+                                paint.ascent() / 2);
+                        canvas.drawText(Integer.toHexString(tile.value), textX, textY, paint);
+                        paint.setTextAlign(Paint.Align.LEFT);
+                    } else {
+                        paint.setColor(Color.argb(255, 192, 192, 192));
+                        canvas.drawRect(tileCoordinateArray[i].startX, tileCoordinateArray[i].startY, tileCoordinateArray[i].endX, tileCoordinateArray[i].endY, paint);
+                    }
+                }
+
+                this.surfaceHolder.unlockCanvasAndPost(canvas);
+            }
+        }
+    }
+
+    private void endGame() {
+
+        Log.d("Syso", "Game ended.....");
     }
 
     private void detectSwipe() {
@@ -88,19 +198,19 @@ public class Twenty48_Engine extends SurfaceView implements Runnable {
                 // South
                 if ((upX - downX) <= (upY - downY)) {
                     // SE -> south
-                    performSwipe("down");
+                    processSwipe("down");
                 } else {
                     // SE -> east
-                    performSwipe("right");
+                    processSwipe("right");
                 }
             } else {
                 // North
                 if ((upX - downX) <= (downY - upY)) {
                     // NE -> north
-                    performSwipe("up");
+                    processSwipe("up");
                 } else {
                     // NE -> east
-                    performSwipe("right");
+                    processSwipe("right");
                 }
             }
         } else {
@@ -109,124 +219,30 @@ public class Twenty48_Engine extends SurfaceView implements Runnable {
                 // South
                 if ((downX - upX) <= (upY - downY)) {
                     // SW -> south
-                    performSwipe("down");
+                    processSwipe("down");
                 } else {
                     // SW -> west
-                    performSwipe("left");
+                    processSwipe("left");
                 }
             } else {
                 // North
                 if ((downX - upX) <= (downY - upY)) {
                     // NW -> north
-                    performSwipe("up");
+                    processSwipe("up");
                 } else {
                     // NW -> west
-                    performSwipe("left");
+                    processSwipe("left");
                 }
             }
         }
     }
 
-    private void performSwipe(String s) {
-        Log.d("Sys out", s);
+    private void processSwipe(String s) {
+
+        Log.d("Syso", s);
     }
 
-    private void draw() {
-        if  (this.surfaceHolder.getSurface().isValid()) {
-            Canvas canvas = this.surfaceHolder.lockCanvas();
-            if (canvas != null) {
-                drawBoard(canvas);
-                drawScoreBoard(canvas);
-                drawUpdateToBoard(canvas);
-                spawnTile();
-            }
-            this.surfaceHolder.unlockCanvasAndPost(canvas);
-        }
-    }
-
-    private void drawUpdateToBoard(Canvas canvas) {
-        for (Object o : tileArray) {
-            if (o != null) {
-                Twenty48_Tile tile = (Twenty48_Tile) o;
-                // draw colored tile
-                paint.setColor(Color.argb(255, tile.color.red, tile.color.green, tile.color.blue));
-                canvas.drawRect(tile.startX, tile.startY, tile.endX, tile.endY, paint);
-                // draw number to tile
-                paint.setColor(Color.DKGRAY);
-                paint.setTextSize(80f);
-                paint.setTextAlign(Paint.Align.CENTER);
-                float textX = tile.startX + ((tile.endX - tile.startX) / 2);
-                float textY = tile.startY + ((tile.endY - tile.startY) / 2) - (paint.descent() + paint.ascent() / 2);
-                canvas.drawText(Integer.toHexString(tile.value), textX, textY, paint);
-                paint.setTextAlign(Paint.Align.LEFT);
-            }
-        }
-    }
-
-    private void drawScoreBoard(Canvas canvas) {
-        int height = canvas.getHeight();
-        int width = canvas.getWidth();
-
-        int titleX = (int) (width * 0.1);
-        int titleY = (int) (height / 4.5);
-
-        int scoreX = (int) (width * 0.51);
-        int scoreY = height / 8;
-        int scoreEndX = (int) (width * 0.7);
-        int scoreEndY = height / 5;
-        int bestX = (int) (width * 0.71);
-        int bestY = height / 8;
-        int bestEndX = (int) (width * 0.9);
-        int bestEndY = height / 5;
-
-        // paints the score elements
-        paint.setColor(Color.argb(255, 216, 216, 216));
-        canvas.drawRect(scoreX, scoreY, scoreEndX, scoreEndY, paint);
-        canvas.drawRect(bestX, bestY, bestEndX, bestEndY, paint);
-        // paints the game name next to the scores
-        paint.setColor(Color.BLACK);
-        paint.setTextSize(40f);
-        paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
-        canvas.drawText("Score", scoreX, scoreY-7, paint);
-        canvas.drawText("Best", bestX, bestY-7, paint);
-
-        paint.setTextSize(128f);
-        canvas.drawText("0x800", titleX, titleY, paint);
-    }
-
-    private void drawBoard(Canvas canvas) {
-        int height = canvas.getHeight();
-        int width = canvas.getWidth();
-
-        int boardWidth = (int) (width * 0.8);
-        int boardHeight = boardWidth;
-        int borderDistance = (width - boardWidth) / 2;
-        int tileLength = (int) (boardHeight / 4.5);
-        int tileOffset = (boardHeight - (tileLength * 4)) / 5;
-
-        paint.setColor(Color.argb(255, 100, 100, 100));
-        // paints the board
-        canvas.drawRect(borderDistance, (height - boardHeight - borderDistance), (width - borderDistance), (height - borderDistance), paint);
-        // paints the tiles
-        paint.setColor(Color.argb(255, 192, 192, 192));
-        int boardX = (width - boardWidth - borderDistance);
-        int boardY = (height - boardHeight - borderDistance);
-        int tileStartX;
-        int tileStartY;
-        int tileEndX;
-        int tileEndY;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4 ; j++) {
-                tileStartX = boardX + (tileOffset * (j + 1)) + (tileLength * j);
-                tileStartY = boardY + (tileOffset * (i + 1)) + (tileLength * i);
-                tileEndX = boardX + (tileOffset * (j + 1)) + (tileLength * (j + 1));
-                tileEndY = boardY + (tileOffset * (i + 1)) + (tileLength * (i + 1));
-                canvas.drawRect(tileStartX, tileStartY, tileEndX, tileEndY, paint);
-                tileCoordinateArray[(i * 4) + j] = new Twenty48_Coordinate(tileStartX, tileStartY, tileEndX, tileEndY);
-            }
-        }
-    }
-
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         switch(event.getActionMasked())
@@ -248,6 +264,7 @@ public class Twenty48_Engine extends SurfaceView implements Runnable {
     @Override
     public void run() {
         while (this.isPlaying) {
+            if (!isInitialized) initialize();
             if (this.nextFrame <= System.currentTimeMillis()) {
                 this.update();
                 this.draw();
@@ -259,12 +276,13 @@ public class Twenty48_Engine extends SurfaceView implements Runnable {
     }
 
     public void pause() {
+
         this.isPlaying = false;
     }
 
     public void resume() {
         this.isPlaying = true;
-        this.thread = new Thread(this);
-        this.thread.start();
+        Thread thread = new Thread(this);
+        thread.start();
     }
 }
---
+++ app/src/main/java/com/gamehub/TwentyFortyEight/Twenty48_UiElement.java
@@ -0,0 +1,14 @@
+package com.gamehub.TwentyFortyEight;
+
+public class Twenty48_UiElement {
+
+    public int startX, startY, endX, endY;
+
+    public Twenty48_UiElement(int startX, int startY, int endX, int endY) {
+        this.startX = startX;
+        this.startY = startY;
+        this.endX = endX;
+        this.endY = endY;
+    }
+
+}

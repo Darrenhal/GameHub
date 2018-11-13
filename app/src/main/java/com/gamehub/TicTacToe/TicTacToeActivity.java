f82222d4ba3a1d8479f7c97166ee3708f6647de2
msngis <44373510+msngis@users.noreply.github.com>
2018-11-13 20:39:55 +0100

    Added several features to TicTacToe

--- app/src/main/java/com/gamehub/TicTacToe/TicTacToeActivity.java
+++ app/src/main/java/com/gamehub/TicTacToe/TicTacToeActivity.java
@@ -1,196 +1,58 @@
 package com.gamehub.TicTacToe;
 
 import android.app.Activity;
-import android.media.AudioAttributes;
-import android.media.AudioManager;
-import android.media.SoundPool;
-import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.gamehub.R;
 
+public class TicTacToeActivity extends Activity {
 
-public class TicTacToeActivity extends Activity implements View.OnClickListener {
+    private TicTacToe_Engine ttt;
 
-    private Button[][] buttons = new Button[3][3];
-
-    private boolean player1Turn = true;
-
-    private int roundCount;
-
-    private int player1Points;
-    private int player2Points;
-
-    private TextView textViewPlayer1;
-    private TextView textViewPlayer2;
-
-    private SoundPool sp;
-    private int soundID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.ttt = new TicTacToe_Engine(this, this);
         setContentView(R.layout.activity_tic_tac_toe);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            AudioAttributes audioAttributes = new AudioAttributes.Builder().
-                        setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
-                        .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                        .build();
-            sp = new SoundPool.Builder()
-                        .setMaxStreams(1)
-                        .setAudioAttributes(audioAttributes)
-                        .build();
-        }   else {
-            sp = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
-        }
-
-        soundID = sp.load(this, R.raw.hitmarker, 1);
-
-
-
-        textViewPlayer1 = findViewById(R.id.text_view_p1);
-        textViewPlayer2 = findViewById(R.id.text_view_p2);
-
-        int resID;
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                String buttonID = "button_"+ i + j;
-                resID = getResources().getIdentifier(buttonID, "id", getPackageName());
-                buttons[i][j] = findViewById(resID);
-                buttons[i][j].setOnClickListener(this);
-            }
-        }
-
-        Button buttonReset = findViewById(R.id.button_reset);
-        buttonReset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resetGame();
-            }
-        });
     }
 
     @Override
-    public void onClick(View v) {
-        sp.play(soundID, 1, 1, 0, 0, 1);
-
-        if (!((Button) v).getText().toString().equals("")) return;
-
-        if (player1Turn) {
-            ((Button) v).setText("X");
-        } else {
-            ((Button) v).setText("O");
-        }
-
-        roundCount++;
-
-        if (checkForWin()) {
-            if (player1Turn) {
-                player1Wins();
-            } else {
-                player2Wins();
-            }
-        } else if (roundCount == 9) {
-            draw();
-        } else {
-            player1Turn = !player1Turn;
-        }
+    public void onResume() {
+        super.onResume();
+        ttt.onResume();
     }
 
-    private boolean checkForWin() {
-        String[][] field = new String[3][3];
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                field[i][j] = buttons[i][j].getText().toString();
-            }
-        }
-
-        for (int i = 0; i < 3; i++) {
-            if (field[i][0].equals(field[i][1])
-                    && field[i][0].equals(field[i][2])
-                    && !field[i][0].equals("")) {
-                return true;
-            }
-
-        }
-
-        for (int i = 0; i < 3; i++) {
-            if (field[0][i].equals(field[1][i])
-                    && field[0][i].equals(field[2][i])
-                    && !field[0][i].equals("")) {
-                return true;
-            }
-        }
-
-        if (field[0][2].equals(field[1][1])
-                && field[0][2].equals(field[2][0])
-                && !field[0][2].equals("")) {
-            return true;
-        }
-
-        if (field[0][0].equals(field[1][1])
-                && field[0][0].equals(field[2][2])
-                && !field[0][0].equals("")) {
-            return true;
-        }
-
-
-        return false;
+    @Override
+    public void onPause() {
+        super.onPause();
+        ttt.onPause();
     }
 
-    private void player1Wins() {
-        player1Points++;
-        Toast.makeText(this, "Player 1 wins!", Toast.LENGTH_SHORT).show();
-        updatePointsText();
-        resetBoard();
+    @Override
+    protected void onStart() {
+        super.onStart();
+
     }
 
-    private void player2Wins() {
-        player2Points++;
-        Toast.makeText(this, "Player 2 wins!", Toast.LENGTH_SHORT).show();
-        updatePointsText();
-        resetBoard();
-    }
-
-    private void draw() {
-        Toast.makeText(this, "Draw!", Toast.LENGTH_SHORT).show();
-        resetBoard();
-    }
-
-    private void updatePointsText() {
-        textViewPlayer1.setText("Player 1: "+ player1Points);
-        textViewPlayer2.setText("Player 2: "+ player2Points);
-    }
-
-    private void resetBoard() {
-        for (int i = 0; i < 3; i++ ) {
-            for (int j = 0; j < 3; j++) {
-                buttons[i][j].setText("");
-            }
-        }
-
-        roundCount = 0;
-        player1Turn = true;
-    }
-
-    private void resetGame() {
-        player1Points = 0;
-        player2Points = 0;
-        updatePointsText();
-        resetBoard();
-    }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        sp.release();
-        sp = null;
+        ttt.onDestroy();
     }
+
+    public void reset(View v) {
+        ttt.resetGame();
+    }
+
+    public void buttonClick(View v) {
+        ttt.buttonClick(v);
+    }
+
 }
---
+++ app/src/main/java/com/gamehub/TicTacToe/TicTacToe_Engine.java
@@ -0,0 +1,297 @@
+package com.gamehub.TicTacToe;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.os.Build;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.gamehub.R;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TicTacToe_Engine extends View  {
+
+    private Context context;
+    private TicTacToeActivity activity;
+
+    private String[][] field;
+
+    private String buttonTag, buttonText, buttonX, buttonY;
+
+    private boolean player1Turn;
+
+    private int roundCount;
+
+    private int player1Points, player2Points;
+
+    private TextView textViewPlayer1, textViewPlayer2;
+
+    private SoundPool spButton;
+    private int tictactoebutton, tictactoebackground, tictactoepossiblewin1, tictactoepossiblewin2, tictactoewin;
+
+    private MediaPlayer mp;
+
+    private Random rdm;
+
+    public TicTacToe_Engine(Context context, TicTacToeActivity activity) {
+        super(context);
+        this.activity = activity;
+
+        field = new String[3][3];
+
+        this.context = context;
+
+        player1Turn = true;
+
+        roundCount = 0;
+
+        player1Points = 0;
+        player2Points = 0;
+
+        field = new String[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+            field[i][j] = "";
+            }
+        }
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            AudioAttributes audioAttributesButton = new AudioAttributes.Builder().
+                    setUsage(AudioAttributes.USAGE_GAME)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .build();
+            spButton = new SoundPool.Builder()
+                    .setMaxStreams(1)
+                    .setAudioAttributes(audioAttributesButton)
+                    .build();
+        }   else {
+            spButton = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        }
+
+
+        tictactoebutton = spButton.load(context, R.raw.tictactoebutton, 1);
+        tictactoepossiblewin1 = spButton.load(context, R.raw.tictactoepossiblewin1, 1);
+        tictactoepossiblewin2 = spButton.load(context, R.raw.tictactoepossiblewin2, 1);
+        tictactoewin = spButton.load(context, R.raw.tictactoewin, 1);
+
+        mp = MediaPlayer.create(activity, R.raw.tictactoebackground);
+        mp.setLooping(true);
+        mp.start();
+
+        rdm = new Random();
+    }
+
+
+
+    public void buttonClick(View v) {
+        spButton.play(tictactoebutton, 1, 1, 0, 0, 1);
+
+        if (!((Button) v).getText().toString().equals("")) return;
+
+        if (player1Turn) {
+            ((Button) v).setText("X");
+        } else {
+            ((Button) v).setText("O");
+        }
+
+        roundCount++;
+
+        if (checkForWin(v)) {
+            if (player1Turn) {
+                player1Wins();
+            } else {
+                player2Wins();
+            }
+        } else if (roundCount == 9) {
+            draw();
+        } else {
+            player1Turn = !player1Turn;
+        }
+    }
+
+    private boolean checkForWin(View v) {
+
+        buttonTag = v.getTag().toString();
+        buttonText = ((Button) v).getText().toString();
+
+        buttonX = buttonTag.substring(0,1);
+        buttonY = buttonTag.substring(1,2);
+
+        field[Integer.parseInt(buttonX)][Integer.parseInt(buttonY)] = buttonText;
+
+        // Man kann frühestens nach 5 Runden gewinnen, bis dahin Ressourcen schonen
+        if(roundCount >= 5) {
+            for (int i = 0; i < 3; i++) {
+                if (field[i][0].equals(field[i][1])
+                        && field[i][0].equals(field[i][2])
+                        && !field[i][0].equals("")) {
+                    return true;
+                }
+            }
+
+            for (int i = 0; i < 3; i++) {
+                if (field[0][i].equals(field[1][i])
+                        && field[0][i].equals(field[2][i])
+                        && !field[0][i].equals("")) {
+                    return true;
+                }
+            }
+
+            if (field[0][2].equals(field[1][1])
+                    && field[0][2].equals(field[2][0])
+                    && !field[0][2].equals("")) {
+                return true;
+            }
+
+            if (field[0][0].equals(field[1][1])
+                    && field[0][0].equals(field[2][2])
+                    && !field[0][0].equals("")) {
+                return true;
+            }
+        }
+        return checkForPossibleWin();
+    }
+
+    public boolean checkForPossibleWin() {
+        int sound = rdm.nextInt(2);
+        Button button;
+        String buttonID;
+        int resID;
+
+        for( int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+
+                if((field[i][j].equals("")
+                        && !field[(i+1)%3][j].equals("")
+                        && field[(i+2)%3][j].equals(field[(i+1)%3][j]))
+
+                    ||
+
+                    (field[i][j].equals("")
+                        && !field[i][(j+1)%3].equals("")
+                        && field[i][(j+2)%3].equals(field[i][(j+1)%3]))
+
+                    ||
+
+                    (field[i][i].equals("")
+                        && !field[(i+1)%3][(j+1)%3].equals("")
+                        && field[(i+2)%3][(j+2)%3].equals(field[(i+1)%3][(j+1)%3])))
+                {
+
+                    buttonID = "button_" + i + j;
+                    resID = activity.getResources().getIdentifier(buttonID, "id", activity.getPackageName());
+                    button = activity.findViewById(resID);
+
+                    final Animation myAnim = AnimationUtils.loadAnimation(activity, R.anim.bounce);
+
+                    MyBounceInterpolator interpolator = new MyBounceInterpolator(10, 100);
+                    myAnim.setInterpolator(interpolator);
+
+                    Log.d("sysout", "favor: " + button.getWidth() + " " + button.getHeight() );
+                    button.setBackgroundColor(Color.argb(255, rdm.nextInt(224)+32, rdm.nextInt(224)+32, rdm.nextInt(224)+32));
+
+                    button.startAnimation(myAnim);
+
+                    mp.pause();
+                    if(sound == 0) {
+                        spButton.play(tictactoepossiblewin1, 1,1,0,0,1);
+                        mp.start();
+                    }
+                    else {
+                        spButton.play(tictactoepossiblewin2, 1,1,0,0,1);
+                    }
+
+                }
+
+            }
+
+        }
+
+
+
+        return false;
+    }
+
+    private void player1Wins() {
+        player1Points++;
+        Toast.makeText(context,"Player 1 wins!", Toast.LENGTH_SHORT).show();
+        updatePointsText();
+        resetBoard();
+    }
+
+    private void player2Wins() {
+        player2Points++;
+        Toast.makeText(context, "Player 2 wins!", Toast.LENGTH_SHORT).show();
+        updatePointsText();
+        resetBoard();
+    }
+
+    private void draw() {
+        Toast.makeText(context, "Draw!", Toast.LENGTH_SHORT).show();
+        resetBoard();
+    }
+
+    private void updatePointsText() {
+        textViewPlayer1 = activity.findViewById(R.id.text_view_p1);
+        textViewPlayer2 = activity.findViewById(R.id.text_view_p2);
+
+        textViewPlayer1.setText("Player 1: " + player1Points);
+        textViewPlayer2.setText("Player 2: " + player2Points);
+
+
+    }
+
+    private void resetBoard() {
+        Button button;
+        for (int i = 0; i < 3; i++ ) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = "";
+                button = (Button) activity.findViewById(getResources().getIdentifier("button_" + i + j,"id" , activity.getPackageName()));
+                button.setText("");
+            }
+        }
+        roundCount = 0;
+        player1Turn = true;
+    }
+
+    public void resetGame() {
+        player1Points = 0;
+        player2Points = 0;
+        updatePointsText();
+        resetBoard();
+    }
+
+    public void onDestroy() {
+        spButton.release();
+        spButton = null;
+
+        mp.stop();
+        mp.release();
+        mp = null;
+    }
+
+    public void onPause() {
+        mp.pause();
+    }
+
+    public void onResume() {
+        mp.start();
+    }
+
+
+}
